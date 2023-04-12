c313cd3191a42b4c4efee6eeca6c972837a284e3
Foo Shee Seng <foosheeseng@ZFS-MB.local>
2023-04-12 11:00:59 +0800

    update to show unique users

--- faq.py
+++ faq.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import uuid
 from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper,  ServiceContext
 from langchain import OpenAI
 import openai
@@ -15,19 +16,27 @@ index = None
 @app.route('/', methods=['GET', 'POST'])
 def search():
     global index
+    print(f"HTTP request received from {request.remote_addr} - {getUniqueName()}")
     if index is None:
         "index not loaded yet. initializing..."
         index = init()
+    
+    if index is None:
+        return render_template('search.html', result={'question': 'Error: index not loaded yet'})
 
     if request.method == 'POST':
         query = request.form['question']
-        # TODO: process query
+
         answer = answerQuestion(query)
         result = {'question': query, 'answer': answer}
 
         return render_template('search.html', result=result)
     else:
         return render_template('search.html')
+
+def getUniqueName():
+    return str(uuid.uuid4())
+    
 
 def is_valid_api_key():
     # Replace YOUR_API_KEY with your actual OpenAI API key
@@ -47,16 +56,16 @@ def is_valid_api_key():
         return True
 
 def init():
+    l_index = None
     #Try to load existing index from disk - sourceIndexFile = os.getcwd() + '/index/' + knowledgeName
-    
     sourceIndexFile = os.getcwd() + '/index/' + knowledgeName
     try:
         print("Loading index from disk...path: " + sourceIndexFile + "")
-        index = GPTSimpleVectorIndex.load_from_disk(sourceIndexFile)
+        l_index = GPTSimpleVectorIndex.load_from_disk(sourceIndexFile)
     except:
-        print("Index not found. Creating new index...")
-        index = createIndex()
-    return index
+        print("Existing index not found in '/index'. Creating new index...")
+        l_index = createIndex()
+    return l_index
 
 def answerQuestion(query):
     print(f"Question: {query}")
@@ -75,16 +84,21 @@ def createIndex():
     chunk_size = 600
     max_chunk_overlap = 20
 
+    #Check API key is valid otherwise exit function
+    if not is_valid_api_key():
+        print("Invalid API key")
+        return
+
     docs = loadKnowledge()
 
     # Get the API key from the environment variable
-    # openai_api_key = os.environ.get("OPENAI_API_KEY")
     api_key = os.environ.get("OPENAI_API_KEY")
 
     llmPredictor = LLMPredictor(llm=OpenAI(api_key=api_key, temperature=0, model_name="text-davinci-003", max_tokens=tokens)) 
     service_context = ServiceContext.from_defaults(llm_predictor=llmPredictor, chunk_size_limit=chunk_size)
     vectorIndex = GPTSimpleVectorIndex.from_documents(docs, service_context=service_context)
 
+    #Save index to disk - /index/knowledge.json
     targetIndexFile = os.getcwd() + '/index/' + knowledgeName
     vectorIndex.save_to_disk(targetIndexFile)
     print("Index saved to disk. path: " + targetIndexFile + "")
